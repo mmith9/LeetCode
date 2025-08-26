3a244ec3df5b310b8638c99ae1c0659f1ce1b880
Adam <adam.wronski@gmail.com>
2025-08-26 16:55:52 +0200

    .

--- 37 Sudoku Solver.py
+++ 37 Sudoku Solver.py
@@ -1,6 +1,4 @@
-from typing import List, Set
-
-
+from typing import List
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
@@ -8,32 +6,36 @@ class Solution:
         board 9x9
         number 1-9 in each row, each column, each 3x3 sub box
         """
+        groups = [set() for _ in range(9)]
+        columns = [set() for _ in range(9)]
+        blanks = []
+        nums_1_to_9 = set(['4','5','6','7','8','9','1','2','3',])
 
-        try_solving(board)
+        for x in range(9):
+            for y in range(9):
+                item = board[y][x]
+                if item == '.':
+                    blanks.append((x,y))
+                else:
+                    groups[y//3*3 + x//3].add(item)
+                    columns[x].add(item)
+        max_pos = len(blanks)
 
-def try_solving(board) -> bool:
-    for y, row in enumerate(board):
-        if '.' in row:
-            x = row.index('.')
-            possible_nums = find_possible_nums(board, y, x)
-            for num in possible_nums:
-                board[y][x] = num
-                if try_solving(board):
-                    return True
-            else:
-                board[y][x] = '.'
-                return False        
-    return True
-
-nums_1_to_9 = set(['1','2','3','4','5','6','7','8','9'])
-def find_possible_nums(board, row, col) -> Set[str]:
-    nums = nums_1_to_9.difference(board[row])
-    for y in range(9):
-        nums.difference_update(board[y][col])
-    sub_row = int(row/3) *3
-    sub_col = int(col/3) *3
-    for y in range(sub_row, sub_row+3):
-        nums.difference_update(board[y][sub_col:sub_col+3])
-    
-    return nums
-    
+        def try_solving(pos) -> bool:
+            if pos >= max_pos:
+                return True
+            x,y = blanks[pos]
+            group = y//3*3 + x//3
+            for num in nums_1_to_9:
+                if num not in columns[x] and num not in board[y] and num not in groups[group]:
+                    board[y][x] = num
+                    columns[x].add(num)
+                    groups[group].add(num)
+                    if try_solving(pos+1):
+                        return True
+                    columns[x].remove(num)
+                    groups[group].remove(num)
+            board[y][x] = '.'        
+            return False
+        
+        try_solving(0)
