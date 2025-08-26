8983dde6e12878d65f72b00a20aeeecf28ad6fcb
John Doe <johndoe@example.com>
2025-08-27 01:04:25 +0200

    .

--- 87 Scramble String.py
+++ 87 Scramble String.py
@@ -1,5 +1,4 @@
 from functools import lru_cache
-from typing import List, Set
 
 class Solution:
     def isScramble(self, s1: str, s2: str) -> bool:
@@ -7,32 +6,26 @@ class Solution:
         # scramble both pieces
         # randomly swap scrambled
         # recursion ends at len of 1
-        #cache = {}
-
-        def is_possible(a,b):
-            lst = set([x for x in a])
-            for letter in b:
-                if letter not in lst:
-                    return False
-                lst.remove(letter)
-            return True
 
         @lru_cache(maxsize=None)
         def scramble(s1, s2):
-            if len(s1) == 1:
-                return s1 == s2
+            lens1 = len(s1)
+            if lens1 <= 2:
+                return (lens1 == 1 and s1 == s2) or (lens1==2 and (s1 == s2 or s1==s2[::-1]))
 
-            if not is_possible(s1, s2):
-                return False
+            a = s1
+            for letter in s2:
+                if letter not in a:
+                    return False
+                a = a.replace(letter,'',1)
 
-            for x in range(1, len(s1)):
+            for x in range(1, lens1):
                 if scramble(s1[:x], s2[:x]) and scramble(s1[x:], s2[x:]):
                     return True
                 
-                s2x = s2[len(s1)-x:] + s2[:len(s1)-x]
-                if scramble(s1[:x], s2x[:x]) and scramble(s1[x:], s2x[x:]):
+                if scramble(s1[:x], s2[lens1-x:]) and scramble(s1[x:], s2[:lens1-x]):
                     return True
             return False
 
         return scramble(s1,s2)
-                
+               
