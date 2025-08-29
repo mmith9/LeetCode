97b7004a94aa1418f36d0f6daea3faaa7928bcf2
Adam <adam.wronski@gmail.com>
2025-08-29 21:37:36 +0200

    .

--- 295 Find Median from Data Stream.py
+++ 295 Find Median from Data Stream.py
@@ -49,39 +49,51 @@ class MedianFinder_1:
 # obj.addNum(num)
 # param_2 = obj.findMedian()
 
-
-# solution that discards values that not fit in the bucket
+# dynamic bucket
+from bisect import insort, bisect_left
 class MedianFinder:
     def __init__(self):
-        self.left = 0
-        self.buckets = [0 for _ in range(101)]
+        self.buckets = {}
         self.bucket_fill = 0
-        self.right = 0
+        self.keys = []
+        self.class_fil = {}
+        self.class_keys = []
+        self.class_width = 100
 
     def addNum(self, num: int) -> None:
-        if 0 <= num <= 100 :
-            self.buckets[num]+=1
-            self.bucket_fill+=1
-        elif num < 0:
-            self.left +=1            
+        if num not in self.buckets:
+            self.buckets[num] = 0
+            insort(self.keys, num)
+        self.buckets[num]+=1
+        self.bucket_fill+=1
+        if num>=0:
+            num_class = int(num/self.class_width)*self.class_width
         else:
-            self.right +=1
+            num_class = int((num-self.class_width+1)/self.class_width)*self.class_width
+        if num_class not in self.class_fil:
+            self.class_fil[num_class] = 0
+            insort(self.class_keys, num_class)
+        self.class_fil[num_class] +=1
 
     def getNum(self, idx:int):
-        lenl = self.left
-        assert idx >= lenl
-        assert idx < self.bucket_fill + lenl
-
-        got_elems = lenl
-        for num, count in enumerate(self.buckets):
-            if idx < got_elems + count:
-                return num
+        got_elems = 0
+        start_index = 0
+        for key in self.class_keys:
+            class_elems = self.class_fil[key]
+            if idx >= got_elems + class_elems:
+                got_elems += class_elems
             else:
-                got_elems += count
-        assert False
+                start_index = bisect_left(self.keys, key)
+                break
+
+        for key in self.keys[start_index:]:
+            got_elems += self.buckets[key]
+            if idx < got_elems:
+                return key
+        return 0 
 
     def findMedian(self) -> float:
-        totals = self.bucket_fill + self.left + self.right
+        totals = self.bucket_fill
         if totals % 2 == 1:
             return self.getNum(totals//2)
         else:
