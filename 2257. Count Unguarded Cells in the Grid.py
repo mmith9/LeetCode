27745b2c89cc17aade9a6117d19ee7c6901738e7
John Doe <johndoe@example.com>
2025-11-02 15:56:58 +0100

    ..

--- 2257. Count Unguarded Cells in the Grid.py
+++ 2257. Count Unguarded Cells in the Grid.py
@@ -1,35 +1,33 @@
 from typing import List
 #Hint1 : brute force it :D
-#optimized brute force, faster than 97% 
+#optimized brute force, beats 100%
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        line = [0]*n
-        board = [line.copy() for _ in range(m)]
+        board = [[0]*n for _ in range(m)]
         for row, col in walls:
             board[row][col] = 2
         for row, col in guards:
             board[row][col] = 2
         for row, col in guards:
             the_row = board[row]
-            for x in range(col+1, n):
-                if the_row[x] == 2:
-                    break
-                the_row[x] = 1
-            for x in range(col-1, -1, -1):
-                    if the_row[x] == 2:
-                        break
-                    the_row[x] = 1
-            for x in range(row+1, m):
-                if board[x][col] == 2:
-                    break
-                board[x][col] = 1
-            for x in range(row-1, -1, -1):
-                if board[x][col] == 2:
-                    break
-                board[x][col] = 1
+            iter = col +1
+            while iter != n and the_row[iter]!=2:
+                the_row[iter] = 1
+                iter+=1
+            iter = col -1
+            while iter != -1 and the_row[iter]!=2:
+                    the_row[iter] = 1
+                    iter -= 1
+            iter = row +1
+            while iter != m and board[iter][col] !=2:
+                board[iter][col] = 1
+                iter += 1
+            iter = row -1
+            while iter != -1 and board[iter][col] !=2:
+                board[iter][col] = 1
+                iter -= 1
         return m*n - sum(sum(row) for row in board) + len(walls) + len(guards)
     
-
 
 engine = Solution()
 m = 4; n = 6; guards = [[0,0],[1,1],[2,3]]; walls = [[0,1],[2,2],[1,4]]
